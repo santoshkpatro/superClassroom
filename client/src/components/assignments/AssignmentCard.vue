23df18a22d982cbf4eb21190fa7615c08a7e85c1
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-09-19 20:52:26 +0530

    assignmnet submission in process

---
+++ client/src/components/assignments/AssignmentCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="box">
+        <h3>{{ assignment.title }}</h3>
+        <p>Submission On - {{ assignment.submission_on }}</p>
+        <br />
+        <h1>Submit your assignment</h1>
+        <div class="file">
+            <label class="file-label">
+                <input
+                    class="file-input"
+                    type="file"
+                    name="file"
+                    id="file"
+                    ref="file"
+                    @change="handleUpload"
+                />
+                <span class="file-cta">
+                    <span class="file-icon">
+                        <i class="fas fa-upload"></i>
+                    </span>
+                    <span class="file-label"> Choose a file… </span>
+                </span>
+            </label>
+            <button @click="submitFile()">Submit</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['classroom_id', 'assignment'],
+    data() {
+        return {
+            file: '',
+        }
+    },
+    methods: {
+        handleUpload() {
+            this.file = this.$refs.file.files[0]
+        },
+        submitFile() {
+            // let formData = new FormData()
+            // formData.append('file', this.file)
+
+            console.log(submitted)
+        },
+    },
+}
+</script>
+
+<style>
+</style>
--- client/src/views/dashboard/AssignmentList.vue
+++ client/src/views/dashboard/AssignmentList.vue
@@ -1,15 +1,22 @@
 <template>
     <div>
-        <div class="box" v-for="assignment in assignments" :key="assignment.id">
-            {{ assignment.title }}
-        </div>
+        <AssignmentCard
+            v-for="assignment in assignments"
+            :key="assignment.id"
+            :assignment="assignment"
+        />
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import AssignmentCard from '../../components/assignments/AssignmentCard.vue'
+
 export default {
     props: ['classroom_id'],
+    components: {
+        AssignmentCard,
+    },
     data() {
         return {
             assignments: null,
