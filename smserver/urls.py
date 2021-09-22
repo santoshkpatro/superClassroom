0ee88a929a7bf236e26a23955910f475f53f4f36
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-09-22 10:55:41 +0530

    updated requirements file to add gunicorn

--- smserver/urls.py
+++ smserver/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('api/auth/', include('api.auth.urls')),
     path('api/classrooms/', include('api.classrooms.urls')),
